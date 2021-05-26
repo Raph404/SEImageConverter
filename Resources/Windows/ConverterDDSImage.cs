5ab8df571282623b75e848f46382e7a9bd7ff96c
Raph404 <38611210+Raph404@users.noreply.github.com>
2021-05-26 21:38:26 +0100

    Update ConverterDDSImage.cs

--- Resources/Windows/ConverterDDSImage.cs
+++ Resources/Windows/ConverterDDSImage.cs
@@ -36,10 +36,13 @@ namespace SEImageConverter.Resources.Windows
             using (MagickImage color = new MagickImage(image.Clone()))
             {
                 int length = Math.Max(color.Height, color.Width) + 2;
+                int width = Math.Min(color.Height, color.Width) + 2;
                 length -= (length % 4);
+                width -= (width % 4);
 
                 color.Format = MagickFormat.Png32;
-                color.Extent(length, length, Gravity.Center, new MagickColor(0, 0, 0, 0));
+                
+                color.Extent(length, width, Gravity.Center, new MagickColor(0, 0, 0, 0));
 
                 color.ColorAlpha(MagickColors.None);
 
